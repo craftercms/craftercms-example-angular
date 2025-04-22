f76adba34596d179853fc461f603f67099e2c719
Phil Nguyen <nquangphuong@gmail.com>
2025-04-22 10:14:09 +0700

    Remove unused util

--- app/src/app/lib/util.ts
+++
@@ -1,8 +0,0 @@
-export function isAuthoring() {
-    const html = document.documentElement;
-    const attr = html.getAttribute('data-is-authoring');
-    return (
-      attr === '${modePreview?c}' || // Otherwise disable/enable if you want to see pencils in dev server.
-      attr === 'true'
-    );
-  }
